4d8373dd857db910ce9b6cb308ff8433eb908960
codenameransom <codejoshuaroque@gmail.com>
2021-07-15 23:35:01 -0400

    commit tree 6c07e2ab9313544a507396f575d7d2a36abe7aa8

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(names, string){
+  return names.filter(function(name){ return name.toLowerCase() === string.toLowerCase()})
+}
+
+function fuzzyMatch(names, string){
+  return names.filter(function(name) {
+    return name.slice(0,2).toLowerCase() === string.toLowerCase();
+  })
+}
+
+function matchName(drivers, name){
+  return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase()
+  )
+}
